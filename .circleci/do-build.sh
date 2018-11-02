a54a54c85ef3cfa67006709ae8906a1e576d34dd
v1993 <v19930312@gmail.com>
2018-11-02 22:48:33 +0300

    Improve build, add .tar.gz pack

--- .circleci/do-build.sh
+++ .circleci/do-build.sh
@@ -4,7 +4,9 @@ set -e
 cmake -H. -Bbuild \
 -DINCLUDE_CPACK=TRUE -DCPACK_GENERATOR=DEB -DCMAKE_INSTALL_PREFIX=/usr \
 -DCMAKE_CXX_COMPILER=clang++ -DCPACK_SET_DESTDIR=TRUE \
--DCPACK_PACKAGE_VERSION="`git log --pretty=format:'%at+git-%H' -n 1`-`lsb_release -cs`"
+-DCPACK_PACKAGE_VERSION="`git log --pretty=format:'%at+git-%H' -n 1`-`lsb_release -cs`" \
+-DBINDIR=/usr/games -DDATADIR=/usr/share/games/cubosphere # Override for standard games location
+
 cmake --build build -- -j4
 
 cmake --build build --target install
---
+++ .circleci/pack-tgz.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Create archive for launching from anywhere
+
+cmake -H. -Bbuild \
+-DINCLUDE_CPACK=TRUE -DCPACK_GENERATOR=TGZ -DCMAKE_INSTALL_PREFIX=/ \
+-DCMAKE_CXX_COMPILER=clang++ -DCPACK_SET_DESTDIR=TRUE \
+-DCPACK_PACKAGE_VERSION="`git log --pretty=format:'%at+git-%H' -n 1`-x64" \
+-DDATADIR=/data -DRUNTIME_DATADIR='./data' -DBINDIR=/ -DINSTALL_DESKTOP=FALSE -DCPACK_ARCHIVE_COMPONENT_INSTALL=TRUE
+
+cmake --build build --target package -- -j4
